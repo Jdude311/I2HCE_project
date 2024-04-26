7f01a5c5588d2e4b54e07b7942a61e8010d908c8
Jdude311 <jadenjohn.ch@gmail.com>
2024-04-26 21:46:27 +0100

    QR Code and MPD connection

--- README.md
+++ README.md
@@ -15,7 +15,11 @@ Python requires a venv setup in this directory to run the qrcode one.
 ## Music for mpd
 Music is stored in the ```~/Music/``` directory. This is where MPD's database is. The music should be in mp3 format.
 
-Playlists can be created in some way but I'm not quite sure how yet.
+Playlists are created by placing files in subfolders of the music directory, and are played with the command ```mpc add``` followed by the name of the folder.
+
+Playlists play by default in alphabetical order. Name the files "1-Song_Title_Here.mp3"
+
+Youtube playlists can be downloaded using ```yt-dlp``` using the following command: ```yt-dlp -x --audio-format mp3 -o "%(playlist_index)d-%(title)s" "https://youtube.com/playlist?list=KJSDJKFHKLSJHDLFKJHSDLKFHJSDKHF"```
 
 ## MPD configuration
 You msut run ```ln -s ~/I2HCE_project/mpd.conf ~/.config/mpd/``` in order to configure MPD correctly.
---
+++ qrcode_mpd_test.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+from signal import pause
+import os
+
+from picamera2 import Picamera2
+from time import sleep
+import cv2
+
+camera = Picamera2()
+camera.start()
+
+detector = cv2.QRCodeDetector()
+
+play_command = "mpc add" 
+
+song_names = {
+        1: "Lionel_Richie",
+        2: "El_Cuarto_De_Tula.mp3",
+        3: "Empire_Ants.mp3",
+        4: "Oblivion.mp3",
+        5: "Strangelove.mp3",
+        6: "Lionel_Richie"
+}
+
+currently_playing = 0
+step_duration = 0.25
+seconds_to_pause = 2
+
+while True:
+    sleep(step_duration)
+    img = camera.capture_array()
+    data, bbox, _ = detector.detectAndDecode(img)
+    print(seconds_to_pause)
+    if data:
+        data = int(data)
+        if data == currently_playing:
+            seconds_to_pause = 3
+            os.system("mpc play")
+        else:
+            currently_playing = data
+            os.system("mpc clear && mpc add \"%s\" && mpc play" % song_names[data])
+    else:
+        if seconds_to_pause <= 0:
+            os.system("mpc pause")
+        else:
+            seconds_to_pause -= step_duration
